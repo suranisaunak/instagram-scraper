81f72f64c9c7668902cf03e563468f2d9f6a0b9e
suranisaunak <suranisaunak@gmail.com>
2019-07-29 17:59:31 +0530

    Instagram hashtag scraper without API

---
+++ config/defaults.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    // URL
+    URL_INSTAGRAM_EXPLORE_TAGS: 'https://www.instagram.com/explore/tags/',
+    URL_INSTAGRAM_MEDIA_SHORTCODE: 'https://www.instagram.com/p/',
+    URL_INSTAGRAM_USER_DATA: 'https://www.instagram.com/',
+
+    // VARIABLES
+    INSTAGRAM_DEFAULT_HASHTAG: 'landscape',
+    INSTAGRAM_DEFAULT_FIRST: 10
+
+};
